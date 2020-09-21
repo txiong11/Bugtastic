5f2ca989d27417c6dc129757501026b56636a8ca
Teng Xiong <txiong11@mail.csuchico.edu>
2020-09-21 11:38:38 -0700

    fix error. Closes #1

--- main.cpp
+++ main.cpp
@@ -5,27 +5,24 @@ int main()
 {
   int age;
   string name;
-  char last initial;
+  char last_initial;
 
-  cout<<"Hi. What is your first name? "
-  cin<<name;
-  cout<<"name, what is the first letter of your last name? ";
-  cin>>last initial;
-  cout<<"Thanks, "<<name<< <<last initial<<.<<endl;
+  cout<<"Hi. What is your first name? ";
+  cin>>name;
+  cout<<name<<", what is the first letter of your last name? ";
+  cin>>last_initial;
+  cout<<"Thanks, "<<name<<last_initial<<"."<<endl;
   cout<<"Please also tell me how old you are: ";
-  cin>>age>>endl;
+  cin>>age;
 
   if( age < 12 )
   {
     cout<<"Hey kid, how do you like school?\n";
   }
-  if( age < 18 )
-    cout<<"Cool!"endl;
-    cout<<"How's highschool going?\n";
-  else( age >= 18 );
-  {
+  if( age > 12 && age < 18 )
+    cout<<"Cool! How's highschool going?\n";
+  else if( age >= 18 )
     cout<<"Pleased to meet you!\n";
-  }
 
   return 0;
 }
